bb54679f2b523de52c1f4ccab749fde924134ec4
Gustavo Ferreira da Silva <gustavofsilvas@gmail.com>
2023-03-08 22:08:35 -0300

    Finalizando inclusão e iniciando apagar
    Função de incluir tasks já feita, agora se iniciou a função de apagar tasks já concluídas

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-let addButton = document.querySelector("#button")
-let taskTitle = document.getElementById("title")
-let taskAbout = document.getElementById("about")
+let addButton = document.getElementById("button")
+let titleArea = document.getElementById("title")
+let aboutArea = document.getElementById("about")
+let tasksArea = document.getElementById("tasks")
+let checkbox = document.getElementsByTagName("input[type=checkbox]")
+let counter = 0
 
-window.alert(addButton+","+taskTitle+","+taskAbout)
-function addTask () {
-    let title = taskTitle.value
-    let about = taskAbout.value
-    window.alert(title,about)
+function addTask() {
+    if(!(titleArea.value == "" || aboutArea.value == "")) {
+        const task = document.createElement("section")
+            task.setAttribute("id","task")
+        const textArea = document.createElement("section")
+        const title = document.createElement("h2")
+            title.innerText = titleArea.value
+        const about = document.createElement("p")
+            about.innerText = aboutArea.value
+        const check = document.createElement("input")
+            check.setAttribute("type","checkbox")
+            check.setAttribute("id","check")
+            check.setAttribute("class",counter)
+            check.setAttribute("onclick","taskDone()")
+
+        task.appendChild(textArea)
+        textArea.appendChild(title)
+        textArea.appendChild(about)
+        task.appendChild(check)
+
+        tasksArea.appendChild(task)
+        
+        titleArea.value = ""
+        aboutArea.value = ""
+        counter++
+    }
+}
+
+function taskDone() {
+    for(let i = 0; i < counter ; i++) {
+        if(checkbox[i].checked == true) {
+            window.alert(checkbox[i])
+        }
+    }
 }
